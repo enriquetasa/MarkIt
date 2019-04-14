aa36ffc6feb50a56163ca6543841863f3332f111
Enrique Tasa <tasa@enriquetasa.com>
2019-04-14 13:47:31 +0200

    Adapter refactor

--- app/src/main/java/com/tarambana/markit/Adapters/SectionsPagerAdapter.java
+++ app/src/main/java/com/tarambana/markit/Adapters/SectionsPagerAdapter.java
@@ -6,20 +6,20 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import com.tarambana.markit.DataContainers.localAssignment;
-import com.tarambana.markit.DataContainers.localSection;
 import com.tarambana.markit.Fragments.MarkStudentFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+// Note that the maximum amount of sections allowed per assignment is 5
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    public localAssignment currentAssignment;
+    public localAssignment currentActiveAssignment;
 
     public SectionsPagerAdapter(FragmentManager fm, localAssignment inputAssignment) {
         super(fm);
-        this.currentAssignment = inputAssignment;
+        this.currentActiveAssignment = inputAssignment;
     }
 
     @Override
@@ -28,22 +28,22 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
         MarkStudentFragment fragment = new MarkStudentFragment();
 
         if (position == 0) {
-            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentAssignment, position + 1));
+            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentActiveAssignment, position + 1));
         } else if (position == 1){
-            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentAssignment, position + 1));
+            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentActiveAssignment, position + 1));
         } else if (position == 2){
-            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentAssignment, position + 1));
+            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentActiveAssignment, position + 1));
         } else if (position == 3) {
-            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentAssignment, position + 1));
+            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentActiveAssignment, position + 1));
         } else {
-            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentAssignment, position + 1));
+            fragment = MarkStudentFragment.newInstance(SplitAssignmentIntoSection(currentActiveAssignment, position + 1));
         }
         return fragment;
     }
 
     @Override
     public int getCount() {
-        return currentAssignment.sectionIDSectionName.size();
+        return currentActiveAssignment.sectionIDSectionName.size();
     }
 
     @Override
@@ -51,15 +51,15 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         switch (position) {
             case 0:
-                return (currentAssignment.assignmentID + ".1");
+                return (currentActiveAssignment.assignmentID + ".1");
             case 1:
-                return (currentAssignment.assignmentID + ".2");
+                return (currentActiveAssignment.assignmentID + ".2");
             case 2:
-                return (currentAssignment.assignmentID + ".3");
+                return (currentActiveAssignment.assignmentID + ".3");
             case 3:
-                return (currentAssignment.assignmentID + ".4");
+                return (currentActiveAssignment.assignmentID + ".4");
             case 4:
-                return (currentAssignment.assignmentID + ".5");
+                return (currentActiveAssignment.assignmentID + ".5");
             default:
                 return null;
         }
@@ -74,7 +74,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         for(int i = 1; i < inputAssignment.partIDSectionID.size(); i++){
 
-            // if you're in here, the section id corresponds to what you want, so assemble the localSection with these
                 List<Integer> partIDList = getAllKeysForValueInHashMap(inputAssignment.partIDSectionID, sectionID);
 
                 for (int j = 0; j < partIDList.size(); j++){
@@ -106,5 +105,3 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     }
 
 }
-
-// Note that the maximum amount of fragments we take is 5
